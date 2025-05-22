b3546e2d1ea99ba73ae5afc0ec2c0be9e80ca1e9
Nick Evil <57915192+Nikisin-git@users.noreply.github.com>
2025-05-22 11:21:02 +0300

    Add files via upload

---
+++ HomeWork2/route_service/app/schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+
+class RouteCreateRequest(BaseModel):
+    """
+    Данные при создании нового маршрута.
+    """
+
+    start_point: str = Field(..., examples=["Москва"])
+    end_point: str = Field(..., examples=["Ярославль"])
+    waypoints: Optional[List[str]] = Field(default=None, examples=["Михнево"])
+    distance: int = Field(..., example=270)
+
+
+class RouteResponse(BaseModel):
+    """
+    Схема ответа для информации о маршруте.
+    """
+
+    route_id: int
+    start_point: str
+    end_point: str
+    waypoints: List[str] = []
+    distance: int
+
+    class Config:
+        from_attributes = True
