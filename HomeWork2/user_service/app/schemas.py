4bba480891033f439666e61be26e593d2a6c2a8d
Nick Evil <57915192+Nikisin-git@users.noreply.github.com>
2025-05-23 21:42:07 +0300

    Add files via upload

---
+++ HomeWork2/user_service/app/schemas.py
@@ -0,0 +1,48 @@
+from pydantic import BaseModel, Field
+
+
+class UserCreateRequest(BaseModel):
+    """
+    Схема для создания пользователя.
+    """
+
+    login: str = Field(..., examples=["johndoe"])
+    password: str = Field(..., examples=["secret"])
+    first_name: str = Field(..., examples=["John"])
+    last_name: str = Field(..., examples=["Doe"])
+    email: str = Field(..., examples=["johndoe@gmail.com"])
+    age: int = Field(..., examples=["21"])
+
+
+class UserResponse(BaseModel):
+    """
+    Схема для ответа.
+    """
+
+    user_id: int
+    login: str
+    first_name: str
+    last_name: str
+    email: str
+    age:int
+
+    class Config:
+        from_attributes = True
+
+
+class TokenRequest(BaseModel):
+    """
+    Схема для запроса токена (логин/пароль).
+    """
+
+    username: str
+    password: str
+
+
+class TokenResponse(BaseModel):
+    """
+    Схема для ответа при успешной аутентификации.
+    """
+
+    access_token: str
+    token_type: str = "bearer"
